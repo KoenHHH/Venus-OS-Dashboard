93432423b9eee1ed68ef73c8fafaa2a038bc003a
KoenH <123877195+KoenHHH@users.noreply.github.com>
2026-02-21 23:12:28 +0100

    Create lang-pt.js

---
+++ dist/lang-pt.js
@@ -0,0 +1,53 @@
+export default {
+  tab1Render: {
+    theme_choice: "Selecionar tema do cartão:",
+    light: "Claro",
+    dark: "Escuro",
+    auto: "Automático",
+    devices_per_column: "Número de \"Dispositivos\" por coluna:",
+    font_size_zones: "Tamanho da fonte nas zonas de \"Dispositivos\":",
+    in_header: "no cabeçalho",
+    in_devices: "nos dispositivos",
+    in_footer: "no rodapé",
+    font_size: "Tamanho da fonte",
+    animation_threshold: "Limiar de animação (W):",
+    animation_threshold_help: "As bolas desaparecem quando a potência está abaixo deste valor",
+    footer_decimals: "Casas decimais do rodapé:",
+    footer_decimals_help: "Número de casas decimais para os valores do rodapé",
+    colors: "Cores:",
+    color_box_bg: "Fundo da caixa",
+    color_dashboard_bg: "Fundo do painel",
+    color_box_shadow: "Sombra / borda da caixa",
+    color_anchor: "Âncora / conector",
+    color_line: "Linha de fluxo"
+  },
+  subtabRender: {
+    header_title: "Configuração do cabeçalho",
+    icon_choice: "Escolher ícone",
+    name_choice: "Nome do dispositivo",
+    sensor_title: "Configuração do sensor principal",
+    entity_choice: "Escolher entidade",
+    entity2_choice: "Escolher entidade secundária",
+    enable_graph: "Ativar gráfico",
+    enable_gauge: "Ativar indicador em %",
+    warning_gauge: "Só pode ser ativado se a unidade de medida for %",
+    header_footer_title: "Configuração dos sensores de cabeçalho e rodapé",
+    entity_header: "Selecionar entidade do cabeçalho",
+    entity_footer: "Selecionar entidade do rodapé",
+    entity2_footer: "Selecionar 2ª entidade do rodapé",
+    entity3_footer: "Selecionar 3ª entidade do rodapé",
+    anchor_title: "Adicionar âncora",
+    left_qtyBox: "Esquerda",
+    top_qtyBox: "Cima",
+    bottom_qtyBox: "Baixo",
+    right_qtyBox: "Direita",
+    add_links: "Adicionar ligações",
+    add_link: "Adicionar ligação"
+  },
+  addLink: {
+    start: "Início",
+    end: "Fim",
+    entity_picker: "Selecionar entidade",
+    reverse: "inverter"
+  }
+};
